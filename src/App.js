a9dbaba6be8553bf7fd51a46f045e997f5386bc9
DELIBINITZ <akshaytupe00@gmail.com>
2022-09-24 22:29:15 +0530

    breaking images

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Register } from "./components/register";
+import { Router } from "@reach/router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Register path="/" />
+        {/* <Home path="/" /> */}
+      </Router>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
+}
+
+export default App;
